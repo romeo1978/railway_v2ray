0ad6feabdead5a237bc26b46dea717d16f100c85
Jerry Tang <romeo1978@qq.com>
2026-02-03 14:55:03 +0800

    Comment out PORT variable in start.sh
    Comment out the PORT variable in start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ CONFIGCADDY=https://raw.githubusercontent.com/romeo1978/railway_v2ray/main/etc/C
 CONFIGXRAY=https://raw.githubusercontent.com/romeo1978/railway_v2ray/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
 StoreFiles=https://raw.githubusercontent.com/romeo1978/railway_v2ray/main/etc/StoreFiles
-PORT=4433
+#PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
